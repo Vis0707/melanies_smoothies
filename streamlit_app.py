fb65400772450944383d86b4181bfa02dc2f2416
Vis0707 <vishv070707@gmail.com>
2024-05-22 14:01:46 -0500

    Update streamlit_app.py
    api call implemented

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,7 @@ import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 
 cnx = st.connection("snowflake")
@@ -26,7 +23,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     st.write(ingredients_string)
